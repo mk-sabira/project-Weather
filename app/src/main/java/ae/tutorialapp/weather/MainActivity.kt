907f668bc63254004017acab07d6843730791241
Sabira <mnsabira@gmail.com>
2021-04-08 16:09:45 +0400

    Button view was discovered

--- app/src/main/java/ae/tutorialapp/weather/MainActivity.kt
+++ app/src/main/java/ae/tutorialapp/weather/MainActivity.kt
@@ -2,8 +2,10 @@ package ae.tutorialapp.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val textView = findViewById<TextView>(R.id.textView)
-//        val editText = findViewById<EditText>(R.id.editText)
-//
-//        textView.setOnClickListener{
-//            textView.text = editText.text
-//        }
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            Toast.makeText(this, getString(R.string.SayHello),Toast.LENGTH_LONG).show()
+        }
     }
 
 }
