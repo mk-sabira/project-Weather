4b718b4802f88403b3040ec2f50cb7a2374a0820
Sabira <mnsabira@gmail.com>
2021-07-13 18:07:08 +0400

    master branch is ready for the project

--- app/src/main/java/ae/tutorialapp/weather/stuff/FragmentItemListener.kt
+++
@@ -1,6 +0,0 @@
-package ae.tutorialapp.weather.stuff
-
-interface FragmentItemListener {
-    fun openFragmentItemDetails(id: Long)
-    fun openBrowser(url: String)
-}
--- app/src/main/java/ae/tutorialapp/weather/stuff/MainActivity.kt
+++ app/src/main/java/ae/tutorialapp/weather/stuff/MainActivity.kt
@@ -8,39 +8,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity(), FragmentItemListener {
-
-
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        setUp()
     }
 
-    private fun setUp() {
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.container, FragmentItems())
-            .addToBackStack(null)
-            .commit()
-    }
-
-    override fun openFragmentItemDetails(id: Long){
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, FragmentItemDetails.newInstance(id))
-            .addToBackStack(null)
-            .commit()
-    }
-
-    override fun openBrowser(url: String) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(intent)
-    }
 
 
 }
